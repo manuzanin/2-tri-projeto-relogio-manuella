5f64bfde927f6da2ab81a06a11f8bcdf8f4e2345
manuzanin <manuella.zanin@escola.pr.gov.br>
2025-06-30 10:43:17 -0300

    aula9

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll (".abas-conteudo");
+const contadores = document.querySelectorAll (".contador");
+const tempoObjetivo1 = new Date ("2025-07-09");
+let tempoAtual = new Date ();
 console.log(botoes)
 
 for(let i=0; i< botoes.length; i++){
@@ -10,3 +14,11 @@ for(let i=0; i< botoes.length; i++){
         botoes[i].classList.add("ativo");
     }
 }
+function calculadoraTempo (tempoObjetivo1){
+    let tempoAtual= new Date();
+    let tempoFinal = tempoObjetivo1-tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+}
+contadores[0].textContent ="Contagem Regressiva";
+contadores[0].textContent = calculadoraTempo (tempoObjetivo1);
+
