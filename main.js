2b93ce728842f7985414f2fc54c870a1dc91bc4d
manuzanin <manuella.zanin@escola.pr.gov.br>
2025-07-04 07:44:14 -0300

    ...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 const botoes = document.querySelectorAll(".botoes");
-const textos = document.querySelectorAll (".abas-conteudo");
-const contadores = document.querySelectorAll (".contador");
+const textos = document.querySelectorAll(".abas-conteudo");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2025-07-09");
 let tempoAtual = new Date ();
 console.log(botoes)
@@ -10,15 +11,20 @@ for(let i=0; i< botoes.length; i++){
         
         for (let j=0; j<botoes.length; j++){
             botoes[j].remove.add("ativo");
+            textos[j].remove.add("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
-function calculadoraTempo (tempoObjetivo1){
-    let tempoAtual= new Date();
+
+function calculadoraTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo1-tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
+
 }
-contadores[0].textContent ="Contagem Regressiva";
+
+contadores[0].textContent = "Contagem Regressiva";
 contadores[0].textContent = calculadoraTempo (tempoObjetivo1);
 
